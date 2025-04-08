46953d99de4da5523edc356867821020b72f4a4c
tanishq8000 <tanishqkhandelwal18072002@gmail.com>
2025-04-08 19:10:34 +0530

    Cors update with production url

--- backend/server.js
+++ backend/server.js
@@ -84,7 +84,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://talk-a-tive-fiz9.onrender.com",
     // credentials: true,
   },
 });
